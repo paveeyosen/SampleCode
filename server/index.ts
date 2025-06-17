e7527a41d09a9db465b056cd0fb8ffec8a319ecd
paveeyosen <38280347+paveeyosen@users.noreply.github.com>
2025-06-17 09:08:23 +0300

    modified index.ts file

--- server/index.ts
+++ server/index.ts
@@ -53,7 +53,8 @@ app.use((req, res, next) => {
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
-    serveStatic(app);
+    // serveStatic(app);
+    await setupVite(app, server);
   }
 
   // ALWAYS serve the app on port 5000
